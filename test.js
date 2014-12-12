f620a186f02aca34470ea6d2e57c809bee46322b
jonschlinkert <dev@sellside.com>
2014-12-12 09:14:06 -0500

    clean up

--- test.js
+++ test.js
@@ -9,15 +9,14 @@
 
 var should = require('should');
 var intersection = require('./');
-var _ = require('lodash');
 
 describe('intersection', function () {
   it('should return unique elements in the given array:', function () {
-    _.intersection(['a', 'a', 'c']).should.eql(['a', 'c']);
+    intersection(['a', 'a', 'c']).should.eql(['a', 'c']);
   });
 
   it('should return only the unique elements from all provided arrays:', function () {
-    _.intersection(['a', 'b', 'c'], ['b', 'c', 'e']).should.eql(['b', 'c']);
-    _.intersection(['a', 'b', 'c'], ['b', 'c', 'e'], ['b', 'c', 'e']).should.eql(['b', 'c']);
+    intersection(['a', 'b', 'c'], ['b', 'c', 'e']).should.eql(['b', 'c']);
+    intersection(['a', 'b', 'c'], ['b', 'c', 'e'], ['b', 'c', 'e']).should.eql(['b', 'c']);
   });
 });
